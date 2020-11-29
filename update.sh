cf6af59b238c64e5a88ab8bc9fbad73b36703b60
sudosubin <sudosubin+github@gmail.com>
2020-11-29 14:11:09 +0900

    Add spicetify update

--- update.sh
+++ update.sh
@@ -128,6 +128,11 @@ update() {
       pull origin master
   fi
 
+  msg_heading "Update spicetify-cli"
+  msg_step "Executing scripts"
+  {
+    curl -fsSL https://raw.githubusercontent.com/khanhas/spicetify-cli/master/install.sh | sh
+  } | output_box cat
 
   # Update miscellaneous
   msg_heading "Update miscellaneous"
